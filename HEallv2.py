f9247315c60555bdc5b47259fedd530005d59ff9
santi <santiago.penate.vera@gmail.com>
2020-02-17 18:45:53 +0100

    Made some changes in format mainly

--- HEallv2.py
+++ HEallv2.py
@@ -5,7 +5,7 @@
 import numpy as np
 import pandas as pd
 from scipy.sparse import csc_matrix
-from scipy.sparse import lil_matrix
+from scipy.sparse import lil_matrix, diags
 from scipy.sparse.linalg import spsolve, factorized
 np.set_printoptions(linewidth=2000)
 # --------------------------- END LIBRARIES
@@ -24,10 +24,8 @@ L = np.zeros((n_linies, n_linies), dtype=complex)
 np.fill_diagonal(L, [1 / (df_top.iloc[i, 2] + df_top.iloc[i, 3] * 1j) for i in range(n_linies)])
 A[df_top.iloc[range(n_linies), 0], range(n_linies)] = 1
 A[df_top.iloc[range(n_linies), 1], range(n_linies)] = -1
-Yx = np.dot(np.dot(A, L), np.transpose(A))
-
-Y = np.zeros((n - 1, n - 1), dtype=complex)  # admittance matrix without slack bus
-Y[:, :] = Yx[1:, 1:]
+Ybus = np.dot(np.dot(A, L), np.transpose(A))
+Ybus = csc_matrix(Ybus)
 
 vecx_shunts = np.zeros((n, 1), dtype=complex)  # vector with shunt admittances
 for i in range(df_top.shape[0]):  # passar per totes les files
@@ -36,38 +34,37 @@ for i in range(df_top.shape[0]):  # passar per totes les files
     vecx_shunts[df_top.iloc[i, 1], 0] = vecx_shunts[df_top.iloc[i, 1], 0] + df_top.iloc[
         i, 4] * -1j  # B/2 is in column 4. The sign is changed here
 
-vec_shunts = np.zeros((n - 1, 1), dtype=complex)  # same vector, just to adapt
+vec_shunts = np.zeros((n - 1), dtype=complex)  # same vector, just to adapt
 for i in range(n - 1):
-    vec_shunts[i, 0] = vecx_shunts[i + 1, 0]
+    vec_shunts[i] = vecx_shunts[i + 1, 0]
 
-# vec_shunts = --vec_shunts  # no need to change the sign, already done
-
-vec_Y0 = np.zeros((n - 1, 1), dtype=complex)  # vector with admittances connecting to the slack
+vec_Y0 = np.zeros(n - 1, dtype=complex)  # vector with admittances connecting to the slack
 
 for i in range(df_top.shape[0]):  # go through all rows
-    if df_top.iloc[i, 0] == 0:  # if slack in the first column
-        vec_Y0[df_top.iloc[i, 1] - 1, 0] = 1 / (
-                    df_top.iloc[i, 2] + df_top.iloc[i, 3] * 1j)  # -1 so bus 1 goes to index 0
-    elif df_top.iloc[i, 1] == 0:  # if slack in the second column
-        vec_Y0[df_top.iloc[i, 0] - 1, 0] = 1 / (df_top.iloc[i, 2] + df_top.iloc[i, 3] * 1j)
 
-G = np.real(Y)  # real parts of Yij
-B = np.imag(Y)  # imaginary parts of Yij
+    if df_top.iloc[i, 0] == 0:  # if slack in the first column
+
+        vec_Y0[df_top.iloc[i, 1] - 1] = 1 / (df_top.iloc[i, 2] + df_top.iloc[i, 3] * 1j)  # -1 so bus 1 goes to index 0
+
+    elif df_top.iloc[i, 1] == 0:  # if slack in the second column
+
+        vec_Y0[df_top.iloc[i, 0] - 1] = 1 / (df_top.iloc[i, 2] + df_top.iloc[i, 3] * 1j)
 
 # --------------------------- INITIAL DATA: Y, SHUNTS AND Y0i. DONE
+
+
 # --------------------------- INITIAL DATA: BUSES INFORMATION
 df_bus = pd.read_excel('Data.xlsx', sheet_name='Busos')  # dataframe of the buses
 if df_bus.shape[0] != n:
     print('Error: número de busos de ''Topologia'' i de ''Busos'' no és igual')  # check if number of buses is coherent
 
-num_busos_PQ = 0  # initialize number of PQ buses
-num_busos_PV = 0  # initialize number of PV buses
-vec_busos_PQ = np.zeros([0], dtype=int)  # vector to store the indices of PQ buses
-vec_busos_PV = np.zeros([0], dtype=int)  # vector to store the indices of PV buses
-vec_P = np.zeros((n - 1, 1), dtype=float)  # data of active power
-vec_Q = np.zeros((n - 1, 1), dtype=float)  # data of reactive power
-vec_V = np.zeros((n - 1, 1), dtype=float)  # data of voltage magnitude
-vec_W = np.zeros((n - 1, 1), dtype=float)  # voltage magnitude squared
+
+pq_list = list()  # vector to store the indices of PQ buses
+pv_list = list()  # vector to store the indices of PV buses
+vec_P = np.zeros(n - 1, dtype=float)  # data of active power
+vec_Q = np.zeros(n - 1, dtype=float)  # data of reactive power
+vec_V = np.zeros(n - 1, dtype=float)  # data of voltage magnitude
+vec_W = np.zeros(n - 1, dtype=float)  # voltage magnitude squared
 
 for i in range(df_bus.shape[0]):  # find the voltage specified for the slack
     if df_bus.iloc[i, 0] == 0:
@@ -75,87 +72,94 @@ for i in range(df_bus.shape[0]):  # find the voltage specified for the slack
     else:
         V_slack = 1
 
+Q0 = np.zeros(n)
+V0 = np.zeros(n)
 for i in range(df_bus.shape[0]):  # store the data of both PQ and PV
     vec_P[df_bus.iloc[i, 0] - 1] = df_bus.iloc[i, 1]  # -1 to start at 0
     if df_bus.iloc[i, 4] == 'PQ':
-        vec_Q[df_bus.iloc[i, 0] - 1] = df_bus.iloc[i, 2]  # -1 to start at 0
-        vec_busos_PQ = np.append(vec_busos_PQ, df_bus.iloc[i, 0])
+        Q0[i] = df_bus.iloc[i, 2]  # -1 to start at 0
+        pq_list.append(i)
     elif df_bus.iloc[i, 4] == 'PV':
-        vec_V[df_bus.iloc[i, 0] - 1] = df_bus.iloc[i, 3]  # -1 to start at 0
-        vec_busos_PV = np.append(vec_busos_PV, df_bus.iloc[i, 0])
-num_busos_PQ = len(vec_busos_PQ)
-num_busos_PV = len(vec_busos_PV)
+        V0[i] = df_bus.iloc[i, 3]  # -1 to start at 0
+        pv_list.append(i)
 
-vec_W = vec_V**2
+
+pq = np.array(pq_list, dtype=int)
+pv = np.array(pv_list, dtype=int)
+pqpv = np.sort(np.r_[pq, pv])
+npq = len(pq)
+npv = len(pv)
+npqpv = npq + npv
+
+vec_Q = Q0[pqpv]
+vec_V = V0[pqpv]
 # --------------------------- INITIAL DATA: BUSES INFORMATION. DONE
 
 # --------------------------- PREPARING IMPLEMENTATION
-prof = 30  # depth
-U = np.zeros((prof, n - 1), dtype=complex)  # voltages
-U_re = np.zeros((prof, n - 1), dtype=complex)  # real part of voltages
-U_im = np.zeros((prof, n - 1), dtype=complex)  # imaginary part of voltages
-X = np.zeros((prof, n - 1), dtype=complex)  # X=1/conj(U)
-X_re = np.zeros((prof, n - 1), dtype=complex)  # real part of X
-X_im = np.zeros((prof, n - 1), dtype=complex)  # imaginary part of X
-Q = np.zeros((prof, n - 1), dtype=complex)  # unknown reactive powers
-pqpv = np.r_[vec_busos_PQ, vec_busos_PV]
-pq = vec_busos_PQ
-pv = vec_busos_PV
-np.sort(pqpv)
-npq = len(pq)
-npv = len(pv)
+prof = 3  # depth
+U = np.zeros((prof, npqpv), dtype=complex)  # voltages
+U_re = np.zeros((prof, npqpv), dtype=float)  # real part of voltages
+U_im = np.zeros((prof, npqpv), dtype=float)  # imaginary part of voltages
+X = np.zeros((prof, npqpv), dtype=complex)  # X=1/conj(U)
+X_re = np.zeros((prof, npqpv), dtype=float)  # real part of X
+X_im = np.zeros((prof, npqpv), dtype=float)  # imaginary part of X
+Q = np.zeros((prof, npqpv), dtype=complex)  # unknown reactive powers
+vec_W = vec_V * vec_V
 dimensions = 2 * npq + 3 * npv  # number of unknowns
 
+Yred = Ybus[np.ix_(pqpv, pqpv)]  # admittance matrix without slack bus
+G = np.real(Yred)  # real parts of Yij
+B = np.imag(Yred)  # imaginary parts of Yij
+
 # .......................CALCULATION OF TERMS [0]
-Y = csc_matrix(Y)
-U[0, :] = spsolve(Y, vec_Y0)
+
+U[0, :] = spsolve(Yred, vec_Y0)
 X[0, :] = 1 / np.conj(U[0, :])
-U_re[0, :] = np.real(U[0, :])
-U_im[0, :] = np.imag(U[0, :])
-X_re[0, :] = np.real(X[0, :])
-X_im[0, :] = np.imag(X[0, :])
+U_re[0, :] = U[0, :].real
+U_im[0, :] = U[0, :].imag
+X_re[0, :] = X[0, :].real
+X_im[0, :] = X[0, :].imag
 # .......................CALCULATION OF TERMS [0]. DONE
 
 # .......................CALCULATION OF TERMS [1]
-valor = np.zeros(n - 1, dtype=complex)
-valor[pq - 1] = (V_slack - 1) * vec_Y0[pq - 1, 0] + (vec_P[pq - 1, 0] - vec_Q[pq - 1, 0] * 1j) * X[0, pq - 1] + U[
-    0, pq - 1] * vec_shunts[pq - 1, 0]
-valor[pv - 1] = (V_slack - 1) * vec_Y0[pv - 1, 0] + (vec_P[pv - 1, 0]) * X[0, pv - 1] + U[0, pv - 1] * vec_shunts[
-    pv - 1, 0]
+valor = np.zeros(npqpv, dtype=complex)
+valor[pq - 1] = (V_slack - 1) * vec_Y0[pq - 1] + (vec_P[pq - 1] - vec_Q[pq - 1] * 1j) * X[0, pq - 1] + U[0, pq - 1] * vec_shunts[pq - 1]
+valor[pv - 1] = (V_slack - 1) * vec_Y0[pv - 1] + (vec_P[pv - 1]) * X[0, pv - 1] + U[0, pv - 1] * vec_shunts[pv - 1]
 
-RHS = np.zeros(2*(n - 1) + npv, dtype=complex)
-RHS[pq - 1] = np.real(valor[pq - 1])
-RHS[pv - 1] = np.real(valor[pv - 1])
-RHS[n - 1 + (pq - 1)] = np.imag(valor[pq - 1])
-RHS[n - 1 + (pv - 1)] = np.imag(valor[pv - 1])
-RHS[2 * (n - 1):] = vec_W[pv - 1, 0] - 1
+RHS = np.zeros(2*(npqpv) + npv, dtype=float)
+RHS[pq - 1] = valor[pq - 1].real
+RHS[pv - 1] = valor[pv - 1].real
+RHS[npqpv + (pq - 1)] = valor[pq - 1].imag
+RHS[npqpv + (pv - 1)] = valor[pv - 1].imag
+RHS[2 * (npqpv):] = vec_W[pv - 1] - 1
 
 
-
-MAT = lil_matrix((dimensions, 2 * (n - 1) + npv), dtype=complex)
-MAT[:(n - 1), :(n - 1)] = G
-MAT[(n - 1):2 * (n - 1), :(n - 1)] = B
+MAT = lil_matrix((dimensions, dimensions), dtype=float)
+MAT[:(npqpv), :(npqpv)] = G
+MAT[(npqpv):2 * (n - 1), :(n - 1)] = B
 MAT[:(n - 1), (n - 1):2 * (n - 1)] = -B
 MAT[(n - 1):2 * (n - 1), (n - 1):2 * (n - 1)] = G
 
-MAT_URE = np.zeros((n - 1, n - 1), dtype=complex)
-np.fill_diagonal(MAT_URE, 2*U_re[0, :])
+MAT_URE = np.zeros((n - 1, n - 1), dtype=float)
+np.fill_diagonal(MAT_URE, 2 * U_re[0, :])
 MAT[2 * (n - 1):, :(n - 1)] = np.delete(MAT_URE, list(pq - 1), axis=0)
 
-MAT_UIM = np.zeros((n - 1, n - 1), dtype=complex)
-np.fill_diagonal(MAT_UIM, 2*U_im[0, :])
+MAT_UIM = np.zeros((n - 1, n - 1), dtype=float)
+np.fill_diagonal(MAT_UIM, 2 * U_im[0, :])
 MAT[2 * (n - 1):, (n - 1):2 * (n - 1)] = np.delete(MAT_UIM, list(pq - 1), axis=0)
 
-MAT_XIM = np.zeros((n - 1, n - 1), dtype=complex)
+MAT_XIM = np.zeros((n - 1, n - 1), dtype=float)
 np.fill_diagonal(MAT_XIM, -X_im[0, :])
 MAT[:(n - 1), 2 * (n - 1):] = np.delete(MAT_XIM, list(pq - 1), axis=1)
 
-MAT_XRE = np.zeros((n - 1, n - 1), dtype=complex)
+MAT_XRE = np.zeros((n - 1, n - 1), dtype=float)
 np.fill_diagonal(MAT_XRE, X_re[0, :])
 MAT[(n-1):2 * (n - 1), 2 * (n - 1):] = np.delete(MAT_XRE, list(pq - 1), axis=1)
 
-MAT_lil = lil_matrix(MAT)
-MAT_csc = factorized(csc_matrix(MAT_lil))
+# factorize (only once)
+MAT_csc = factorized(MAT.tocsc())
+
+# solve
 LHS = MAT_csc(RHS)
 
 U_re[1, :] = LHS[:(n - 1)]
@@ -164,8 +168,8 @@ Q[0, pv - 1] = LHS[2 * (n - 1):]
 
 U[1, :] = U_re[1, :] + U_im[1, :] * 1j
 X[1, :] = (-X[0, :] * np.conj(U[1, :])) / np.conj(U[0, :])
-X_re[1, :] = np.real(X[1, :])
-X_im[1, :] = np.imag(X[1, :])
+X_re[1, :] = X[1, :].real
+X_im[1, :] = X[1, :].imag
 
 # .......................CALCULATION OF TERMS [1]. DONE
 
@@ -183,18 +187,17 @@ def conv(A, B, c, i, tipus):
         suma = [A[k, i] * np.conj(B[c - k, i]) for k in range(1, c)]
         return sum(suma)
 
+
 for c in range(2, prof):  # c defines the current depth
 
-    valor[pq - 1] = (vec_P[pq - 1, 0] - vec_Q[pq - 1, 0] * 1j) * X[c - 1, pq - 1] + U[c - 1, pq - 1] * vec_shunts[
-        pq - 1, 0]
-    valor[pv - 1] = conv(X, Q, c, pv - 1, 2) * (-1) * 1j + U[c - 1, pv - 1] * vec_shunts[pv - 1, 0] + \
-        X[c - 1, pv - 1] * vec_P[pv - 1, 0]
+    valor[pq - 1] = (vec_P[pq - 1] - vec_Q[pq - 1] * 1j) * X[c - 1, pq - 1] + U[c - 1, pq - 1] * vec_shunts[pq - 1]
+    valor[pv - 1] = conv(X, Q, c, pv - 1, 2) * -1j + U[c - 1, pv - 1] * vec_shunts[pv - 1] + X[c - 1, pv - 1] * vec_P[pv - 1]
 
-    RHS[pq - 1] = np.real(valor[pq - 1])
-    RHS[pv - 1] = np.real(valor[pv - 1])
-    RHS[n - 1 + (pq - 1)] = np.imag(valor[pq - 1])
-    RHS[n - 1 + (pv - 1)] = np.imag(valor[pv - 1])
-    RHS[2 * (n - 1):] = -conv(U, U, c, pv - 1, 3)
+    RHS[pq - 1] = valor[pq - 1].real
+    RHS[pv - 1] = valor[pv - 1].real
+    RHS[n - 1 + (pq - 1)] = valor[pq - 1].imag
+    RHS[n - 1 + (pv - 1)] = valor[pv - 1].imag
+    RHS[2 * (n - 1):] = -conv(U, U, c, pv - 1, 3).real  # the convolution of 2 complex is real :)
 
     LHS = MAT_csc(RHS)
 
@@ -211,18 +214,45 @@ for c in range(2, prof):  # c defines the current depth
 # --------------------------- CHECK DATA
 U_final = np.zeros(n - 1, dtype=complex)  # final voltages
 U_final[0:n - 1] = U.sum(axis=0)
-I_serie = Y * U_final  # current flowing through series elements
-I_inj_slack = vec_Y0[:, 0] * V_slack
+I_serie = Yred * U_final  # current flowing through series elements
+I_inj_slack = vec_Y0 * V_slack
 I_shunt = np.zeros((n - 1), dtype=complex)  # current through shunts
-I_shunt[:] = -U_final[:] * vec_shunts[:, 0]  # change the sign again
+I_shunt[:] = -U_final * vec_shunts  # change the sign again
 I_generada = I_serie - I_inj_slack + I_shunt  # current leaving the bus
-I_gen2 = [(vec_P[i, 0] - vec_Q[i, 0] * 1j) / np.conj(U_final[i]) if i + 1 in pq else
-          (vec_P[i, 0] - sum(Q[:, i] * 1j)) / np.conj(U_final[i]) for i in range(n - 1)]
 
-print(U_final)
-print(abs(U_final))
-print(I_gen2 - I_generada)  # current balance. Should be almost 0
+# assembly the reactive power vector
+Qfinal = vec_Q.copy()
+Qfinal[pv-1] = (Q[:, pv-1] * 1j).sum(axis=0).imag
+
+# compute the current injections
+I_gen2 = (vec_P - vec_Q * 1j) / np.conj(U_final)
+
+# print(U_final)
+# print(abs(U_final))
+# print(I_gen2 - I_generada)  # current balance. Should be almost 0
 Qdf = pd.DataFrame(Q)  # to check the unknown reactive power
 Qdf.to_excel('Results_reactive_power.xlsx', index=False, header=False)
 Udf = pd.DataFrame(U)
 Udf.to_excel('Results_voltage_coefficients.xlsx', index=False, header=False)  # to check the voltages
+
+df = pd.DataFrame(np.c_[np.abs(U_final), np.angle(U_final), Qfinal, np.abs(I_gen2 - I_generada)],
+                  columns=['|V|', 'Angle', 'Q', 'I error'])
+print(df)
+# test
+
+V_test = np.array([0.95368602,
+                   0.94166879,
+                   0.93910714,
+                   0.95,
+                   0.94,
+                   0.92973537,
+                   0.93579263,
+                   0.91,
+                   0.94618528,
+                   0.98,
+                   0.92])
+
+ok = np.isclose(abs(U_final), V_test, atol=1e-3).all()
+
+if not ok:
+    print('Test failed')
